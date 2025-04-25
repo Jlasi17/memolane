f19be3939d6fdd0ae8038c76748633a2e701f415
lasyaJ <116539458+Jlasi17@users.noreply.github.com>
2025-04-25 23:11:44 +0530

    new main.py

--- memoweb/server/main.py
+++ memoweb/server/main.py
@@ -63,11 +63,10 @@ Path(UPLOAD_DIR2).mkdir(parents=True, exist_ok=True)
 # CORS Configuration
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://jlasi17.github.io"],  # Your React app URL
+    allow_origins=["https://jlasi17.github.io/memolane","http://localhost:3000"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
-    expose_headers=["*"]
 )
 
 # MongoDB Configuration
